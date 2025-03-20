aedd38c6faf3cb982195e49a139c82201431fd57
pmmenezes <protopedro@gmail.com>
2025-03-20 00:18:39 -0300

    PrimeNG

--- stock.client/src/app/app.module.ts
+++ stock.client/src/app/app.module.ts
@@ -1,19 +1,33 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//app.module
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+//primeng
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+//
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
+    BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
